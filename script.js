efa0fab433ade4d5f248b53a28dee0770598538d
Marri Balasatish <128178005+BalaSatish03@users.noreply.github.com>
2024-08-22 16:33:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+// Selecting the textarea element to display the calculator's result
+const display = document.querySelector('textarea');
+
+// Function to clear the display
+function clearDisplay() {
+    display.value = '';
+}
+
+// Function to append a value to the display
+function appendToDisplay(value) {
+    display.value += value;
+}
+
+// Function to calculate the result
+function calculate() {
+    if(display.value==''){
+        display.value=''
+    }
+    else{
+   
+        // Evaluate the expression in the display
+    display.value = eval(display.value);}}
+    
+
+function clearValue() {
+    display.value = display.value.slice(0, -1);
+}
+
+
+// Function to handle button clicks
+function handleButtonClick(event) {
+    const buttonValue = event.target.innerText;
+
+    if (buttonValue === 'AC') {
+        clearDisplay();
+    } else if (buttonValue === '=') {
+        calculate();
+    } else if (buttonValue=='⌫'){
+        clearValue();
+    }
+    else {
+        appendToDisplay(buttonValue);
+    }
+}
+
+// Adding event listeners to each table cell (button)
+const buttons = document.querySelectorAll('td');
+buttons.forEach(button => {
+    button.addEventListener('click', handleButtonClick);
+});
